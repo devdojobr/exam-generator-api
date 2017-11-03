b46916e15bb22eb61f0ba47aba145b416c2ffef4
William Suane <williamsuane@bitbucket.org>
2017-11-03 16:27:01 -0400

    [video 26] finished tests for course

--- src/test/java/br/com/devdojo/examgenerator/endpoint/v1/course/CourseEndpointTest.java
+++ src/test/java/br/com/devdojo/examgenerator/endpoint/v1/course/CourseEndpointTest.java
@@ -5,6 +5,7 @@ import br.com.devdojo.examgenerator.persistence.model.Course;
 import br.com.devdojo.examgenerator.persistence.respository.CourseRepository;
 import br.com.devdojo.examgenerator.persistence.respository.ProfessorRepository;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Collections;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 /**
  * @author William Suane for DevDojo on 11/3/17.
@@ -27,10 +31,10 @@ import static org.junit.Assert.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class CourseEndpointTest {
+    //MAJOR.MINOR.PATCH
+    //4.2.2
     @MockBean
     private CourseRepository courseRepository;
-    @MockBean
-    private ProfessorRepository professorRepository;
     @Autowired
     private TestRestTemplate testRestTemplate;
     private HttpEntity<Void> professorHeader;
@@ -51,16 +55,97 @@ public class CourseEndpointTest {
         HttpHeaders headers = testRestTemplate.postForEntity("/login", body, String.class).getHeaders();
         this.professorHeader = new HttpEntity<>(headers);
     }
+
     @Before
     public void configWrongHeader() {
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization","1111");
-        this.professorHeader = new HttpEntity<>(headers);
+        headers.add("Authorization", "1111");
+        this.wrongHeader = new HttpEntity<>(headers);
     }
+
     @Before
-    public void setup(){
+    public void setup() {
         BDDMockito.when(courseRepository.findOne(course.getId())).thenReturn(course);
         BDDMockito.when(courseRepository.listCourses("")).thenReturn(Collections.singletonList(course));
         BDDMockito.when(courseRepository.listCourses("java")).thenReturn(Collections.singletonList(course));
     }
+
+    @Test
+    public void getCourseByIdWhenTokenIsWrongShouldReturn403() throws Exception {
+        ResponseEntity<String> exchange = testRestTemplate.exchange("/v1/professor/course/1", GET, wrongHeader, String.class);
+        assertThat(exchange.getStatusCodeValue()).isEqualTo(403);
+    }
+
+    @Test
+    public void listCoursesWhenTokenIsWrongShouldReturn403() throws Exception {
+        ResponseEntity<String> exchange = testRestTemplate.exchange("/v1/professor/course/list?name=", GET, wrongHeader, String.class);
+        assertThat(exchange.getStatusCodeValue()).isEqualTo(403);
+    }
+
+    @Test
+    public void listAllCoursesWhenNameDoesNotExistsShouldReturn404() throws Exception {
+        ResponseEntity<String> exchange = testRestTemplate.exchange("/v1/professor/course/list?name=xaxa", GET,
+                professorHeader, String.class);
+        assertThat(exchange.getStatusCodeValue()).isEqualTo(404);
+    }
+
+    @Test
+    public void listAllCoursesWhenNameExistsShouldReturn200() throws Exception {
+        ResponseEntity<String> exchange = testRestTemplate.exchange("/v1/professor/course/list?name=java", GET, professorHeader, String.class);
+        assertThat(exchange.getStatusCodeValue()).isEqualTo(200);
+    }
+
+    @Test
+    public void getCourseByIdWithoutIdShouldReturn400() throws Exception {
+        ResponseEntity<String> exchange = testRestTemplate.exchange("/v1/professor/course/", GET, professorHeader, String.class);
+        assertThat(exchange.getStatusCodeValue()).isEqualTo(400);
+    }
+
+    @Test
+    public void getCourseByIdWhenCourseIdDoesNotExistsShouldReturn404() throws Exception {
+        ResponseEntity<String> exchange = testRestTemplate.exchange("/v1/professor/course/-1", GET, professorHeader, String.class);
+        assertThat(exchange.getStatusCodeValue()).isEqualTo(404);
+    }
+
+    @Test
+    public void getCourseByIdWhenCourseExistsShouldReturn200() throws Exception {
+        ResponseEntity<String> exchange = testRestTemplate.exchange("/v1/professor/course/1", GET, professorHeader, String.class);
+        assertThat(exchange.getStatusCodeValue()).isEqualTo(200);
+    }
+
+    @Test
+    public void deleteCourseWhenIdExistsShouldReturn200() throws Exception {
+        long id = 1L;
+        BDDMockito.doNothing().when(courseRepository).delete(id);
+        ResponseEntity<String> exchange = testRestTemplate.exchange("/v1/professor/course/{id}", GET, professorHeader, String.class, id);
+        assertThat(exchange.getStatusCodeValue()).isEqualTo(200);
+    }
+
+    @Test
+    public void deleteCourseWhenIdDoesNotExistsShouldReturn404() throws Exception {
+        long id = -1L;
+        BDDMockito.doNothing().when(courseRepository).delete(id);
+        ResponseEntity<String> exchange = testRestTemplate.exchange("/v1/professor/course/{id}", GET, professorHeader, String.class, id);
+        assertThat(exchange.getStatusCodeValue()).isEqualTo(404);
+    }
+
+    @Test
+    public void createCourseWhenNameIsNullShouldReturn400() throws Exception {
+        Course course = courseRepository.findOne(1L);
+        course.setName(null);
+        assertThat(createCourse(course).getStatusCodeValue()).isEqualTo(400);
+    }
+    @Test
+    public void createCourseWhenEverythingIsRightShouldReturn200() throws Exception {
+        Course course = courseRepository.findOne(1L);
+        course.setId(null);
+        assertThat(createCourse(course).getStatusCodeValue()).isEqualTo(200);
+    }
+
+    private ResponseEntity<String> createCourse(Course course) {
+        BDDMockito.when(courseRepository.save(course)).thenReturn(course);
+        return testRestTemplate.exchange("/v1/professor/course/", POST,
+                new HttpEntity<>(course, professorHeader.getHeaders()), String.class);
+    }
+
 }
