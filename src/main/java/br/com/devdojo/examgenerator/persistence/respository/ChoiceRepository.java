e333e0542c4761486e18b3ff60e689cc951465f5
William Suane <williamsuane@bitbucket.org>
2017-11-20 15:41:56 -0500

    [video 47] CRUD choice pt 03 front end list

--- src/main/java/br/com/devdojo/examgenerator/persistence/respository/ChoiceRepository.java
+++ src/main/java/br/com/devdojo/examgenerator/persistence/respository/ChoiceRepository.java
@@ -10,9 +10,9 @@ import java.util.List;
  * @author William Suane for DevDojo on 11/20/17.
  */
 public interface ChoiceRepository extends CustomPagingAndSortRepository<Choice, Long> {
-    @Query("select c from Choice c where c.question.id = ?1 and q.professor = ?#{principal.professor} and q.enabled = true")
+    @Query("select c from Choice c where c.question.id = ?1 and c.professor = ?#{principal.professor} and c.enabled = true")
     List<Choice> listChoicesByQuestionId(long questionId);
 
-    @Query("update Choice c set c.correctAnswer = false where c <> ?1 and c.question = ?2 and q.professor = ?#{principal.professor} and q.enabled = true")
+    @Query("update Choice c set c.correctAnswer = false where c <> ?1 and c.question = ?2 and c.professor = ?#{principal.professor} and c.enabled = true")
     void updateAllOtherChoicesCorrectAnswerToFalse(Choice choice, Question question);
 }
