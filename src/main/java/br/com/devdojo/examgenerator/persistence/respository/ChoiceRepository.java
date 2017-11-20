3ce6145d12c19212e0211547cef79e54f30094ba
William Suane <williamsuane@bitbucket.org>
2017-11-20 16:35:13 -0500

    [video 48] CRUD choice pt 04 added @modifying

--- src/main/java/br/com/devdojo/examgenerator/persistence/respository/ChoiceRepository.java
+++ src/main/java/br/com/devdojo/examgenerator/persistence/respository/ChoiceRepository.java
@@ -2,6 +2,7 @@ package br.com.devdojo.examgenerator.persistence.respository;
 
 import br.com.devdojo.examgenerator.persistence.model.Choice;
 import br.com.devdojo.examgenerator.persistence.model.Question;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -14,5 +15,6 @@ public interface ChoiceRepository extends CustomPagingAndSortRepository<Choice, 
     List<Choice> listChoicesByQuestionId(long questionId);
 
     @Query("update Choice c set c.correctAnswer = false where c <> ?1 and c.question = ?2 and c.professor = ?#{principal.professor} and c.enabled = true")
+    @Modifying
     void updateAllOtherChoicesCorrectAnswerToFalse(Choice choice, Question question);
 }
